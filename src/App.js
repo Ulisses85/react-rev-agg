8a7cfe2b817c9b1984aa5ecc35a519507ad4e668
Lukasz Kuligowski <lukasz.kuligowski@wsp.com>
2020-06-13 12:41:08 +0100

    feat: check prettier code formatter

--- src/App.js
+++ src/App.js
@@ -2,32 +2,32 @@ import React, { Component } from "react";
 
 import "./App.css";
 
-const formatNumber = (number) => new Intl.NumberFormat("en", { minimumFractionDigits: 2 }).format(number);
+const formatNumber = (number) =>
+  new Intl.NumberFormat("en", { minimumFractionDigits: 2 }).format(number);
 class App extends Component {
   render() {
     return (
       <div class="product-list">
         <label>Search Products</label>
         <input type="text" />
-        
+
         <table>
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Revenue</th>
-          </tr>
-        </thead>
-        <tbody>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td>Total</td>
-            <td></td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
-  );
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Revenue</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+          <tfoot>
+            <tr>
+              <td>Total</td>
+              <td></td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
   }
 }
 
